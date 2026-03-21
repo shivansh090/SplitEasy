3e05bc79dfac51346d014f3ddd2fbafe03bbe43f
shivansh.singh <shivansh.singh@cashfree.com>
2026-03-22 00:31:38 +0530

    removed cors issue

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,15 +1,5 @@
 const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      'http://localhost:5173',
-      'http://localhost:3000',
-    ];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
